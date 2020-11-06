1b7231569e167485505bd6353b0af6d75e90e423
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2020-11-06 14:47:12 +0800

    Rough layout of home screen - Added the coloured text for headers - Constrained most things down

--- I'm Cat/ViewController.swift
+++ I'm Cat/ViewController.swift
@@ -9,9 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var headerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setUpHeaderLabel()
+        
+    }
+    
+    func setUpHeaderLabel() {
+        let headerAttributedString = NSMutableAttributedString(string: "Me = 😺\nYou = 😺\nEveryone = 😺",
+                                                               attributes: [.font : UIFont.boldSystemFont(ofSize: 40),
+                                                                            .foregroundColor : UIColor.accentColor])
+        
+        try! NSRegularExpression(pattern: "=", options: []).enumerateMatches(in: headerAttributedString.string,
+                                                                        options: [],
+                                                                        range: NSRange(location: 0,
+                                                                                       length: headerAttributedString.length)) { (result, _, _) in
+            
+            headerAttributedString.addAttribute(.foregroundColor, value: UIColor.label, range: result!.range)
+        }
+        
+        headerLabel.attributedText = headerAttributedString
     }
 
 
